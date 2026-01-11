9a192bf1365d5270ce4f292c7a36d125e6848dd9
schurupik72 <schurupik72@gmail.com>
2026-01-11 13:08:02 +0300

    Re-enable UserApiTest class for execution

--- src/test/java/com/automation/tests/UserApiTest.java
+++ src/test/java/com/automation/tests/UserApiTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 
 
-@Disabled
+//@Disabled
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserApiTest extends BaseApiTest{
 
